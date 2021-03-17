4961357940b0fad8477a4039534f1221df812e70
samahri <saud.amahri@gmail.com>
2021-03-17 16:09:40 -0700

    add SecurityConfig Class

---
+++ src/main/java/guru/sfg/brewery/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package guru.sfg.brewery.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // permit the request with permitAll(), otherwise, prompt a username/password
+        http
+            .authorizeRequests(authorize -> {
+                authorize
+                        .antMatchers("/").permitAll()
+                        .antMatchers("/webjars/**", "/login", "resources/**").permitAll()
+                        .antMatchers("/beers/find", "/beers*").permitAll();
+            })
+            .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+            .formLogin().and()
+            .httpBasic();
+    }
+}
