a293678c8ccf72256e512de681e5f3ef22a2913c
samahri <saud.amahri@gmail.com>
2021-03-17 16:20:16 -0700

    add Rest mvcMatcher

--- src/main/java/guru/sfg/brewery/config/SecurityConfig.java
+++ src/main/java/guru/sfg/brewery/config/SecurityConfig.java
@@ -20,6 +20,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/webjars/**", "/login", "resources/**").permitAll()
                 .antMatchers("/beers/find", "/beers*").permitAll()
                 .antMatchers(HttpMethod.GET, "/api/v1/beer/**").permitAll()
+                .mvcMatchers(HttpMethod.GET, "api/v1/beerUpc/{upc}").permitAll()
             .and()
             .authorizeRequests()
                 .anyRequest().authenticated()
